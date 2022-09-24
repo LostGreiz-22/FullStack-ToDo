ba4c73c33b23f820f81007d52c75362b0d1d2526
LostGreiz-22 <lnnnkojp@gmail.com>
2022-09-24 13:44:53 +0800

    Initial Commit

---
+++ src/main/java/fullstack/fullstacktodo/FullStackToDoApplication.java
@@ -0,0 +1,13 @@
+package fullstack.fullstacktodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackToDoApplication.class, args);
+	}
+
+}
